69530e828b6eb09dfa6c5a724ddce795d6e70416
Mothannahuss <mothannahuss@gmail.com>
2024-04-18 16:25:47 +0300

    myProfile and sign up done

--- scripts/profileScript.js
+++ scripts/profileScript.js
@@ -51,4 +51,43 @@ document.addEventListener("DOMContentLoaded", function()
     })
 
 
+    document.querySelector("#all").addEventListener("click", function()
+    {
+        let div = document.querySelector("#contentHolder");
+        div.innerHTML = `      <div class="event-card">
+        <div class="event-avatar"></div>
+        <div class="event-content">
+            <div class="event-details">
+                <p class="event-club">Club Name</p>
+
+                <h2 class="event-title">Club Name</h2>
+                <div class="event-info">
+                    <p><i class="fas fa-calendar-alt"></i> Day, Date</p>
+                    <p><i class="fas fa-clock"></i> Time</p>
+                    <p><i class="fas fa-map-marker-alt"></i> Location</p>
+                </div>
+            </div>
+            <button class="view-event-btn">View Event</button>
+        </div>
+    </div>
+
+    <div class="event-card">
+      <div class="event-avatar"></div>
+      <div class="event-content">
+          <div class="event-details">
+              <p class="event-club">Club Name</p>
+
+              <h2 class="event-title">Club Name</h2>
+              <div class="event-info">
+                  <p><i class="fas fa-calendar-alt"></i> Day, Date</p>
+                  <p><i class="fas fa-clock"></i> Time</p>
+                  <p><i class="fas fa-map-marker-alt"></i> Location</p>
+              </div>
+          </div>
+          <button class="view-event-btn">View Event</button>
+      </div>
+  </div>`;
+    })
+
+
 })
