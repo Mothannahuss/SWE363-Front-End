fa2bb5ead63bd30051556a4a106df6982deeb03e
tada.TOT <abdulghani612kh@gmail.com>
2024-05-14 22:02:45 +0300

    "f"

--- server.js
+++ server.js
@@ -84,10 +84,6 @@ app.use("*", (req, res) => {
     });
 });
 
-
-
-
-
 async function indexClubsToMeiliSearch() {
     const items = await Club.find(); // Fetch all items from MongoDB
     const documents = items.map(item => ({
@@ -100,11 +96,6 @@ async function indexClubsToMeiliSearch() {
     await index.addDocuments(documents);
   }
 
-
-
-
-
-
 async function search(client)
 {
     let query = "Computer";
@@ -115,91 +106,6 @@ async function search(client)
       } catch (err) {
         console.error(err);
       }
-}
-
-
-async function setUp()
-{
-    let cs = new User({
-        email: "cs@kfupm.edu.sa",
-        password: await bcrypt.hash("12345678", 10),
-        is_club: true,
-        interests: ["Coding", "Computer Science"],
-        following: ["Computer Club"],
-    })
-
-    let vd =  new User({
-        email: "vd@kfupm.edu.sa",
-        password: await bcrypt.hash("12345678", 10),
-        is_club: true,
-        interests: ["Video Games"],
-        following: ["Video Games Club"],
-    });
-
-    await cs.save();
-    await vd.save();
-
-
-    let csClub = new Club({
-        name: "Computer Club",
-        user: cs._id,
-        handler:"@csClub",
-        bio:"bla bla",
-        about:"bla bla",
-        categories: ["Programming"]
-    })
-
-    let vdClub = new Club({
-        name: "Video Games Club",
-        user: vd._id,
-        handler:"@vdClub",
-        bio:"bla bla",
-        about:"bla bla",
-        categories: ["gaming"]
-    });
-
-    await csClub.save();
-    await vdClub.save();
-
-    let csEvent = new Event({
-        club_id: csClub._id,
-        club_name: csClub.name,
-        title: "JS help session",
-        date: new Date().toISOString(),
-        location: "Bldg 22-119",
-        description: "NodeJS help session",
-    })
-
-
-    let vdEvent = new Event({
-        club_id: vdClub._id,
-        club_name: vdClub.name,
-        title: "FIFA tournament",
-        date: new Date().toISOString(),
-        location: "Bldg 22-119",
-        description: "EA24 competition",
-    });
-
-    await csEvent.save();
-    await vdEvent.save();
-
-
-    let u = new User({
-        email: "mothannahuss@gmail.com",
-        password: await bcrypt.hash("12345678", 10),
-        interests: ["Coding", "biking"],
-        following: ["Computer Club"],
-        refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6eyJkYXRlIjoiMjAyNC0wNS0xM1QxNTo1MzoyMC4yMzhaIiwiY2x1YiI6ZmFsc2V9LCJpYXQiOjE3MTU2MTU3ODcsImV4cCI6MTcxNTcwMjE4N30.OMyFxBIyemomRWUHTJPjFDGWv5RRVKZdUUsARKTHKWU"
-    });
-
-    await u.save();
-
-    let n1 = new Notification({
-        event: csEvent._id,
-        user: u._id,
-    })
-
-    await n1.save();
 }
 
 try {
