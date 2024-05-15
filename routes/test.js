5c8e242ff301737c07e6be4f7ae7a383d898aac4
Omar Aljohani <omerrr350@hotmail.com>
2024-05-15 08:59:27 +0300

    Added editprofile page

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const clubHandler = require("../models/Club.js")
 
 router.post("/", userController.toggleClubFollow);
+router.get("/:club_handler", async (req, res) =>{
+    let clubs = await clubHandler.findOne({handler: {$eq: req.params["club_handler"]}})
+    res.render("editprofile", {club: clubs})
+})
 
 module.exports = router;
